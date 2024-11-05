25f7225d85b1abacf05c1a8e136e612870697974
Cosimo Di Lorenzo <cosimodilorenzo1997@gmail.com>
2024-11-05 17:48:38 +0100

    Inizio esercizio teatro

---
+++ python/05.11/esercizio teatro/posto.py
@@ -0,0 +1,16 @@
+class Posto:
+    def __init__(self):
+        self.__numero = 1
+        self.__fila = "prima fila"
+        self.__occupato = True
+
+    def get_numero(self):
+        return self.__numero
+    
+    def get_fila(self):
+        return self.__fila
+
+
+
+    
+    
